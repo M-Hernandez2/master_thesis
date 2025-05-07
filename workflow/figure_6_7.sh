b65261f39cea859f86a30460fe3ab60146067ad4
M-Hernandez2 <157639568+M-Hernandez2@users.noreply.github.com>
2025-05-07 11:35:30 -0400

    fix path

--- workflow/figure_6_7.sh
+++ workflow/figure_6_7.sh
@@ -5,7 +5,7 @@ conda activate my_envir
 
 
 # (1) create folders with all outputs of 500 runs
-PY_STR_PLOT="python ../NEW_inputs/Final_Runs/CONCENTRATIONS/figure_6_7.py"
+PY_STR_PLOT="python ./figure_6_7.py"
 SRUN_OPTS_PLOT="--exclusive --nodes=1 --ntasks=1 --cpus-per-task=1"
 srun $SRUN_OPTS_PLOT $PY_STR_PLOT &
 wait
